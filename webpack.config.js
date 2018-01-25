ceaaf9474987197e27e3f80557d2902c131a65c8
Dario Segura <dsegura@uncharted.software>
2018-01-25 18:02:56 -0500

    [BUILD] Added method to import powerbi utilities + imported formatting utility.

--- webpack.config.js
+++ webpack.config.js
@@ -3,8 +3,15 @@ const path = require('path');
 const ENTRY = './src/FacetsVisual.ts';
 const regex = path.normalize(ENTRY).replace(/\\/g, '\\\\').replace(/\./g, '\\.');
 
+const POWERBI_UTILS = [
+    './node_modules/globalize/lib/cultures/globalize.culture.en-US.js',
+    './node_modules/powerbi-visuals-utils-typeutils/lib/index.js',
+    './node_modules/powerbi-visuals-utils-dataviewutils/lib/index.js',
+    './node_modules/powerbi-visuals-utils-formattingutils/lib/index.js'
+];
+
 module.exports = {
-    entry: ENTRY,
+    entry: POWERBI_UTILS.concat(ENTRY),
     devtool: 'eval',
     resolve: {
         extensions: ['', '.webpack.js', '.web.js', '.js', '.ts'],
@@ -23,6 +30,16 @@ module.exports = {
             {
                 test: new RegExp(regex),
                 loader: path.join(__dirname, 'bin', 'pbiPluginLoader'),
+            },
+            {
+                test: /\bpowerbi\b.*?\butils\b.*?\bindex\.js\b/,
+                loader: 'string-replace',
+                query: {
+                    multiple: [
+                        { search: 'var powerbi;', replace: 'var powerbi = window.powerbi;' },
+                        { search: 'var Globalize = Globalize || window["Globalize"];', replace: 'var Globalize = require(\'globalize\');' },
+                    ]
+                }
             },
             {
                 test: /\.ts?$/,
